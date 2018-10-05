25eb88517b7a42488e43a73983059c75c6c78036
Tomáš Beňo <benjiko99@gmail.com>
2018-10-05 13:42:34 +0200

    Extract multi-lang support into own files

---
+++ multi-lang.js
@@ -0,0 +1,39 @@
+var currentLang = "sk"
+
+$(function() {
+  setLanguage(currentLang)
+  $("#switch-lang").click(toggleLanguage)
+});
+
+function toggleLanguage() {
+  if (currentLang == "sk") {
+    setLanguage("en")
+  } else if (currentLang == "en") {
+    setLanguage("sk")
+  }
+}
+
+function setLanguage(language) {
+  console.log("Setting language: " + language)
+
+  currentLang = language
+  $(".container").attr("lang", language)
+
+  if (language == "sk") {
+    $("#switch-lang").text("Read in English")
+    findElementsByLanguage("sk").show()
+  } else {
+    findElementsByLanguage("sk").hide()
+  }
+
+  if (language == "en") {
+    $("#switch-lang").text("Čítať v Slovenčine")
+    findElementsByLanguage("en").show()
+  } else {
+    findElementsByLanguage("en").hide()
+  }
+}
+
+function findElementsByLanguage(language) {
+  return $(".container :lang(" + language + ")")
+}
